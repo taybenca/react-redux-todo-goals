eb52a7e5ab7a66b31a907d641ba0172b6afaaf33
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-12-05 17:16:08 +0000

    Create the pure function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// This is a pure function because it is not modifying the state
+function todos (state = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+}
+
 function createStore () {
     // The store should have four parts
     // 1. The state
