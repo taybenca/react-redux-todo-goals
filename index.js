7eee7cc478d9f20fd53dda79368f5edae2b111bb
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-12-06 12:06:52 +0000

    Add cases for remove todo and toggle todo

--- index.js
+++ index.js
@@ -35,12 +35,23 @@ function createStore (reducer) {
 
 // App code
 // This is a pure function because it is not modifying the state
+// This is a reducer function
 function todos (state = [], action) {
-    if (action.type === 'ADD_TODO') {
-        return state.concat([action.todo])
+    switch(action.type) {
+        case 'ADD_TODO' :
+            return state.concat([action.todo])
+        case 'REMOVE_TODO' :
+            return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO' :
+            return state.map((todo) => 
+                todo.id !== action.id ? 
+                todo : 
+                Object.assign({}, todo, { complete: !todo.complete })
+                //create a new object, add all the todo apart of the complete
+                )
+        default :
+            return state
     }
-
-    return state
 }
 
 const store = createStore(todos)
